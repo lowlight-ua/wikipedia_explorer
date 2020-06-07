ce3f750a476248633e66c86f640bba5d7162e667
Yaroslav Pidstryhach <wikiexp@yp.lviv.ua>
2020-06-07 17:36:12 +0300

    class Article

--- wikipedia_explorer.js
+++ wikipedia_explorer.js
@@ -1,25 +1,48 @@
 'use strict';
 
-let appl = {};
+let expl = {};
 
 // ----------------------------------------------------------------------------
 
 class Article
 {
-    constructor(title)
+    constructor(value)
     {
-        this.title = title;
+        this.title = value;
+        console.log(this);
+    }
+
+    get title() { return this.title_; }
+    set title(value)
+    {
+        if (value.length == 0)
+        {
+            throw new Error("Empty article title");
+        }
+        this.title_ = value;
+    }
+
+    get pageid() { return this.pageid_; }
+    set pageid(value) 
+    {
+        if(!value) { throw new Error("Empty pageid"); }
+        this.pageid_ = value;
     }
 }
 
 // ----------------------------------------------------------------------------
 
-class Application
+class Explorer
 {
     constructor()
     {
         this.articles = {};
         this.steps = 0;
+    }
+
+    run(title)
+    {
+        this.articles[title] = new Article(title);
     }
 
     onStepBegin()
