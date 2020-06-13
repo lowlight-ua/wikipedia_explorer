56b92f025a8c249d21a7a2ed963494752770aaff
Yaroslav Pidstryhach <wikiexp@yp.lviv.ua>
2020-06-13 20:18:44 +0300

    Graphviz refactoring

---
+++ output.js
@@ -0,0 +1,56 @@
+function generateDot(model) {
+    function cTrim(c) {
+        return c.substring(9);
+    }
+
+    function printCategory(c) {
+        const fontSize = 8 + (c.articles.size + c.children.size) * 0.6;
+        dot += ('"' + cTrim(c.title) + '" [fontsize=' + fontSize + ']\n');
+    }
+
+    let dot = new String();   
+    dot += ('digraph { \nrankdir="LR" ' +
+        'nodesep=0.3 \n' + 
+        'node [fontname="Helvetica"]\n' +
+        'node [shape=box height=0.4 fontsize=10 style=filled fillcolor="#e0e0e0"]\n' +
+        'edge [dir=none]\n');
+    let ctr = 0;
+    
+    // Nodes
+    for (const [ctitle, c] of Object.entries(model.categories)) {
+        printCategory(c);
+        for(const p of c.parents) {
+            const pObj = model.categories[p];
+            if(pObj) {
+                printCategory(pObj);
+            }
+        }
+    }
+    dot += ('nodesep=0.1 ' + 
+        'node [shape=none height=0 fontsize=8 style=filled fillcolor="#f0f0f0"]\n');
+        
+    for (const [ctitle, c] of Object.entries(model.categories)) {
+        for(const a of c.articles) {
+            const yellow = a==this.title ? '[fillcolor="#FFFFB0"]' : '';
+            dot += ('"' + a + ' (' + ctr + ')" ' + yellow + '\n');
+            ctr++;
+        }
+    }
+
+    ctr = 0;
+    // Edges
+    for (const [ctitle, c] of Object.entries(model.categories)) {
+        for(const p of c.parents) {
+            if(model.categories[p]) {
+                dot += ('"' + cTrim(p) + '" -> "' + cTrim(ctitle) + '"\n');
+            }
+        }
+        for(const a of c.articles) {
+            dot += ('"' + cTrim(ctitle) + '" -> "' + a + ' (' + ctr + ')" [color="#B0B0B0"]\n');
+            ctr++;
+        }
+    }
+    dot += ('}');
+
+    return dot;
+}
--- wikipedia_explorer.js
+++ wikipedia_explorer.js
@@ -41,59 +41,8 @@ class Explorer
 
     onCategoryTreeBuilt() {
         pruneModel(this.model, this.title);
-
-        console.log("Phase 3 done");     
-        let dot = new String();   
-        dot += ('digraph { \nrankdir="LR" ' +
-            'nodesep=0.3 \n' + 
-            'node [fontname="Helvetica"]\n' +
-            'node [shape=box height=0.4 fontsize=10 style=filled fillcolor="#e0e0e0"]\n' +
-            'edge [dir=none]\n');
-        let ctr = 0;
-        function cTrim(c) {
-            return c.substring(9);
-        }
-        function printCategory(c) {
-            const fontSize = 8 + (c.articles.size + c.children.size) * 0.6;
-            dot += ('"' + cTrim(c.title) + '" [fontsize=' + fontSize + ']\n');
-        }
-        
-        // Nodes
-        for (const [ctitle, c] of Object.entries(this.model.categories)) {
-            printCategory(c);
-            for(const p of c.parents) {
-                const pObj = this.model.categories[p];
-                if(pObj) {
-                    printCategory(pObj);
-                }
-            }
-        }
-        dot += ('nodesep=0.1 ' + 
-            'node [shape=none height=0 fontsize=8 style=filled fillcolor="#f0f0f0"]\n');
-            
-        for (const [ctitle, c] of Object.entries(this.model.categories)) {
-            for(const a of c.articles) {
-                const yellow = a==this.title ? '[fillcolor="#FFFFB0"]' : '';
-                dot += ('"' + a + ' (' + ctr + ')" ' + yellow + '\n');
-                ctr++;
-            }
-        }
-
-        ctr = 0;
-        // Edges
-        for (const [ctitle, c] of Object.entries(this.model.categories)) {
-            for(const p of c.parents) {
-                if(this.model.categories[p]) {
-                    dot += ('"' + cTrim(p) + '" -> "' + cTrim(ctitle) + '"\n');
-                }
-            }
-            for(const a of c.articles) {
-                dot += ('"' + cTrim(ctitle) + '" -> "' + a + ' (' + ctr + ')" [color="#B0B0B0"]\n');
-                ctr++;
-            }
-        }
-        dot += ('}');
-
+        const dot = generateDot(this.model);
         $("#output").append(dot);
+        console.log("Phase 3 done");     
     }
 }
